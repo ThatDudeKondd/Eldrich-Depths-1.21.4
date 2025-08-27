7e57ad42b70e85e72aac53836f0617e19cd8ba51
[Konstantinos Domouzis] <[konstantinosdd@gmail.com]>
2025-08-27 18:57:51 +0200

    Trying stuff.

--- src/main/java/net/thatdudekondd/eldrich_depths/item/ModItems.java
+++ src/main/java/net/thatdudekondd/eldrich_depths/item/ModItems.java
@@ -4,8 +4,6 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -15,6 +13,7 @@ import java.util.function.Function;
 
 public class ModItems {
     public static final Item WARDEN_HEAD = registerItem("warden_head", Item::new, new Item.Settings());
+    public static final Item CLEAVER = registerItem("cleaver", Item::new, new Item.Settings());
 
     private static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(EldrichDepths.MOD_ID,name));
@@ -26,6 +25,9 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(WARDEN_HEAD);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(CLEAVER);
         });
     }
 }
